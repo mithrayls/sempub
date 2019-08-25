ff695534c8510b833665daaee2055a68dbe674a7
abc123 <abc123@gmail.com>
2019-08-26 11:14:52 +1200

    italic

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ async function promptDetails(){
 			let version = publication_details.version
 			let type = publication_details.release_type
 
-			confirmation[0].message = `Do you wish to publish the ${c.blue(type)} release with version number ${version}?`
+			confirmation[0].message = `Do you wish to publish the ${c.italic.blue(type)} release with version number ${version}?`
 
 			return publication_details
 	  })
