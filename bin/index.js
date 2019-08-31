2e6e33d0638ba0db3e61c92a1a66569adf9fc729
abc123 <abc123@gmail.com>
2019-08-31 22:56:54 +1200

    test

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,21 @@
 #! /usr/bin/node
 
 const program = require('commander')
-const config = require('../package.json')
-const sempub = require('../index.js')
+const yaml		= require('yaml')
+
+const config 				= require('../package.json')
+const sempub 				= require('../index.js')
+const command_file	= require('./commands.yml')
+const commands 			= yaml.parse(fs.readFileSync( command_file, 'utf8'))
 
 program
 
 	.version(config.version)
 
-	.command( 'init')
-	.description('initialize configuration')
-	.action( sempub.init )
+	commands.forEach( command => {
+		.command( 'init')
+		.description('initialize configuration')
+		.action( sempub.init )
+	})
 
 program.parse(process.argv)
