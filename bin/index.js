b3301dca7f27148033b7aa56b87cd36c86d03303
mithrayls <mithray@protonmail.com>
2019-09-13 00:12:50 +1200

    switch to emiln

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
-//const emiln = require('emiln')
+const emiln = require('emiln')
 
 let commands = [
     Object.values(require('../spec/git.js')),
     Object.values(require('../spec/npm.js')),
     Object.values(require('../spec/main.js'))
 ]
-/*
 emiln(commands, process.argv).then(async res => {
     console.log(await res)
     process.exit(1)
 })
-*/
